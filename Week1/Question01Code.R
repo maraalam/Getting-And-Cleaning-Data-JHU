cf2e92b0266cf82b8f5016d66fa93a7b89a0ff16
María Cristina Alameda Salas <60483752+maraalam@users.noreply.github.com>
2022-11-10 19:41:30 +0100

    Update Question01Code.R

--- Week1/Question01Code.R
+++ Week1/Question01Code.R
@@ -1,7 +1,7 @@
 library(data.table)
 
 if(!file.exists("data/IdahoHousingData.csv")){
-        fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv "
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
         download.file(url = fileUrl, destfile = "data/IdahoHousingData.csv")
         dateDownloaded <- date()
 }
