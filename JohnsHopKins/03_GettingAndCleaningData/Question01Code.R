fc8f8fd30f30dbda25f115aa20b729a2f0879c3b
María Cristina Alameda Salas <60483752+maraalam@users.noreply.github.com>
2022-11-10 19:17:46 +0100

    Create Question01Code.R

---
+++ JohnsHopKins/03_GettingAndCleaningData/Question01Code.R
@@ -0,0 +1,13 @@
+library(data.table)
+
+if(!file.exists("IdahoHousingData.csv")){
+        fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv "
+        download.file(url = fileUrl, destfile = "IdahoHousingData.csv")
+        dateDownloaded <- date()
+}
+
+data <- read.csv("data/IdahoHousingData.csv")
+head(data)
+
+count <- sum(data$VAL == 24, na.rm = TRUE)
+count
